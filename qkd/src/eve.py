e5f51fd0b8acb2b13be7b03eaf7607c9ae85301f
poig <43441314+poig@users.noreply.github.com>
2022-04-11 23:41:33 +0800

    Update eve.py

--- qkd/src/eve.py
+++ qkd/src/eve.py
@@ -1,10 +1,13 @@
 from netqasm.sdk.qubit import Qubit
+import random
+from netqasm.sdk.external import NetQASMConnection
+#from epr_socket import DerivedEPRSocket as EPRSocket
 
 class Eve:
 
     def __init__(self):
         pass
-
+    
     def eavesdrop(self, qubit: Qubit):
         # IMPLEMENT YOUR EAVESDROPPING CODE HERE
         #
@@ -17,4 +20,24 @@ class Eve:
         # When measuring make sure to use `inplace=True` when calling
         # `qubit.measure(inpace=True)` so that the qubit is still available when
         # delivered to the application. Otherwise, the qubit is released.
-        pass
+
+        #no_one_time_pad
+        q = qubit
+        n=1
+        #bit_flips = [None for _ in range(n)]
+        key_string = [random.randint(1, 3) for i in range(n)]
+        for i in range(n):
+            if key_string[i] == 1:
+                q.H()
+            elif key_string[i] == 2:
+                q.S()
+                q.H()
+                q.T()
+                q.H()
+            #q.flush()
+            
+            m = q.measure(inplace=True)
+            #bit_flips[i] = int(m)
+            #q.flush()
+        return q
+        #pass
