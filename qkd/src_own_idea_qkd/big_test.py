d3d76d7d243665d38671db8394344a11dbe36634
poig <43441314+poig@users.noreply.github.com>
2022-04-13 14:13:30 +0800

    Update big_test.py

--- qkd/src_own_idea_qkd/big_test.py
+++ qkd/src_own_idea_qkd/big_test.py
@@ -23,3 +23,32 @@ print('max:',max(list_res))
 print('avg:',sum(list_res)/len(list_res))
 print('min:',min(list_res))
 print('finish!')
+
+
+#use to cdo eve check
+#need to add 'None if error_rate > 20.00 else' to return secret_key
+'''import subprocess 
+list_res = []
+for i in range(100):
+    subprocess.run(["qne", "experiment", "run", "exp", "--timeout",  "30"])
+
+    with open("/root/qne-qchack-2022/qkd/exp/raw_output/LAST/results.yaml",'r') as f:
+        f.readline()
+        f.readline()
+        f.readline()
+        ok = f.readline()
+    print(i)
+    #print(ok)
+    if not('null' in ok):
+        list_res.append('no_eveee')
+    else:
+        list_res.append('eveeev')
+    #print(i)
+print(list_res)
+ii = 0
+for i in range(len(list_res)):
+    if list_res[i] == 'eveeev':
+        ii+=1
+print('eve success:',ii)
+#print(max(list_res))
+print('finish!')'''
